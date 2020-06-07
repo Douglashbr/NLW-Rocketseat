94824bdf9c84fd97dad95176feafc7590208442d
Douglas Dantas <douglas.mdantas@hotmail.com>
2020-06-07 16:50:11 -0300

    Last day

--- server/src/controller/PointsController.ts
+++ server/src/controller/PointsController.ts
@@ -18,7 +18,7 @@ class PointsController {
         const trx = await knex.transaction();
 
         const point = {
-            image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            image: req.file.filename,
             name,
             email,
             whatsapp,
@@ -32,11 +32,14 @@ class PointsController {
     
         const point_id = insertedIds[0];
     
-        const pointItems = items.map((item_id: number) => {
-            return {
-                item_id,
-                point_id
-            }
+        const pointItems = items
+            .split(',')
+            .map((item: string) => Number(item.trim()))
+            .map((item_id: number) => {
+                return {
+                    item_id,
+                    point_id
+                }
         })
     
         await trx('point_items').insert(pointItems);
@@ -58,12 +61,17 @@ class PointsController {
             return res.status(400).json({ message: 'Point not found.' })
         }
 
+        const serializedPoint = {
+            ...point,
+            image_url: `http://192.168.0.2:3333/uploads/${point.image}`
+        }
+
         const items = await knex('items')
             .join('point_items', 'items.id', '=', 'point_items.item_id')
             .where('point_items.point_id', id)
             .select('items.title');
 
-        return res.json({ point, items });
+        return res.json({ serializedPoint, items });
     }
 
     async index(req: Request, res: Response) {
@@ -81,7 +89,14 @@ class PointsController {
             .distinct()
             .select('points.*');
 
-        return res.json(points)
+        const serializedPoints = points.map(point => {
+            return {
+                ...points,
+                image_url: `http://192.168.0.2:3333/uploads/${point.image}`
+            }
+        })
+
+        return res.json(serializedPoints)
     }
 }
 
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -6,6 +6,8 @@ import { LeafletMouseEvent } from 'leaflet'
 import api from '../../services/api';
 import axios from 'axios';
 
+import Dropzone from '../../components/Dropzone';
+
 import './styles.css'
 
 import logo from '../../assets/logo.svg';
@@ -41,6 +43,7 @@ const CreatePoint = () => {
         whatsapp: ''
     });
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     const history = useHistory();
 
@@ -117,7 +120,6 @@ const CreatePoint = () => {
 
     async function handleSubmit(e: FormEvent) {
         e.preventDefault();
-        console.log('dshaudhsa')
 
         const { name, email, whatsapp } = formData;
         const uf = selectedUf;
@@ -125,15 +127,19 @@ const CreatePoint = () => {
         const [latitude, longitude] = selectPosition;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
+        const data = new FormData();
+
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+
+        if (selectedFile) {
+            data.append('image', selectedFile);
         }
 
         await api.post('points', data);
@@ -155,6 +161,8 @@ const CreatePoint = () => {
 
             <form onSubmit={handleSubmit}>
                 <h1>Cadatro do <br/> ponto de coleta</h1>
+
+                <Dropzone onFileUploaded={setSelectedFile} />
 
                 <fieldset>
                     <legend>
